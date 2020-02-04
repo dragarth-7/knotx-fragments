a78c583ce60ae2577b7a6b3b6888a751936f3877
marcin.szymura <marcin.szymura@cognifide.com>
2020-02-04 12:46:53 +0100

    #73 Annotate HttpActionFactory as Cacheable

--- handler/actions/src/main/java/io/knotx/fragments/handler/action/http/HttpActionFactory.java
+++ handler/actions/src/main/java/io/knotx/fragments/handler/action/http/HttpActionFactory.java
@@ -18,9 +18,11 @@ package io.knotx.fragments.handler.action.http;
 import io.knotx.fragments.handler.action.exception.ActionConfigurationException;
 import io.knotx.fragments.handler.api.Action;
 import io.knotx.fragments.handler.api.ActionFactory;
+import io.knotx.fragments.handler.api.Cacheable;
 import io.vertx.core.Vertx;
 import io.vertx.core.json.JsonObject;
 
+@Cacheable
 public class HttpActionFactory implements ActionFactory {
 
   @Override
